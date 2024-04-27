84f95ea0e4e32475ed7cf959d83f2012d7ffda82
litpa <aslitpa@gmail.com>
2024-04-27 17:25:14 +0700

    Fix images not being displayed on the recipe page

--- frontend/src/components/pages/RecipePage/RecipeInstruction/Step/Images/Images.jsx
+++ frontend/src/components/pages/RecipePage/RecipeInstruction/Step/Images/Images.jsx
@@ -1,10 +1,11 @@
 import "./Images.css";
+import Img from "../../../../../UI/Image/Img";
 
 const Images = (props) => {
     return <div className="images">
         {
-            props.images.map((image, index) =>
-                <img src={image.imageLink} key={index}/>
+            props.images.map((imageLink, index) =>
+                <Img src={imageLink} key={index} width={"30%"} height={180}/>
             )
         }
     </div>;
