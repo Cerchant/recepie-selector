7dbd7bc89897a3f56048087ab4be192bcea7f838
litpa <aslitpa@gmail.com>
2024-04-27 17:26:16 +0700

    Add spacing and margins on the recipe page

--- frontend/src/components/pages/RecipePage/RecipeInstruction/Step/Step.jsx
+++ frontend/src/components/pages/RecipePage/RecipeInstruction/Step/Step.jsx
@@ -2,11 +2,10 @@ import "./Step.css";
 import Images from "./Images/Images";
 
 const Step = (props) => {
-    return <div>
+    return <div className="step">
         <h3>Шаг {props.stepNumber}</h3>
         <p>{props.text}</p>
         <Images images={props.images}/>
-        {console.log(props.images)}
     </div>;
 }
 
