4f44673e70afa205b2cdb114733ffd3292d4bec6
litpa <aslitpa@gmail.com>
2024-05-23 23:02:06 +0700

    add age, weight and height validation on ext-registration page

--- frontend/src/components/pages/ExtendedRegistrationPage/ExtendedRegistrationForm/ExtendedRegistrationForm.jsx
+++ frontend/src/components/pages/ExtendedRegistrationPage/ExtendedRegistrationForm/ExtendedRegistrationForm.jsx
@@ -107,17 +107,17 @@ const ExtendedRegistrationForm = (props) => {
 
   const ageInputChangeHandler = (e) => {
     setAgeInput(e.target.value);
-    setAgeIsValid(+e.target.value > 0);
+    setAgeIsValid(e.target.value >= 3 && e.target.value <= 130);
   };
 
   const heightInputChangeHandler = (e) => {
     setHeightInput(e.target.value);
-    setHeightIsValid(+e.target.value > 0);
+    setHeightIsValid(e.target.value >= 60 && e.target.value <= 270);
   };
 
   const weightInputChangeHandler = (e) => {
     setWeightInput(e.target.value);
-    setWeightIsValid(+e.target.value > 0);
+    setWeightIsValid(e.target.value >= 8 && e.target.value <= 700);
   };
 
 
