90c7a20f96fe7af2579c4eedc0c3e61fd409e328
Андрей <zz11926557@gmail.com>
2024-05-25 17:31:43 +0700

    add date pickers to profile page for kbju table, normal request url for history

--- frontend/src/components/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/components/pages/ProfilePage/ProfilePage.jsx
@@ -10,12 +10,56 @@ import ProfileForm from "./ProfileForm/ProfileForm";
 import styles from "./ProfilePage.module.css";
 import axios from "axios";
 import ProfileTable from "./ProfileTable/ProfileTable";
+import dayjs from "dayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 const ProfilePage = (props) => {
   const history = useHistory();
   const token = localStorage.getItem("token");
   const [user, setUser] = useState();
   const [recipes, setRecipes] = useState([]);
+  const [startDate, setStartDate] = useState(
+    dayjs().hour(0).minute(0).second(0)
+  );
+  const [endDate, setEndDate] = useState(
+    dayjs().hour(23).minute(59).second(59)
+  );
+
+  const fetchRecipes = async () => {
+    let recipesData;
+    try {
+      recipesData = (
+        await axios.post(
+          "http://127.0.0.1:8000/business/history",
+          {
+            start_date_time: startDate.toISOString(),
+            end_date_time: endDate.toISOString(),
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+      ).data;
+
+      console.log(recipesData);
+      const matchedData = recipesData.map((data) => ({
+        id: data.rid,
+        name: data.rname,
+        k: data.kbju.k,
+        b: data.kbju.b,
+        j: data.kbju.j,
+        u: data.kbju.u,
+      }));
+      setRecipes(matchedData);
+    } catch (ex) {
+      const { response } = ex;
+      console.log(response);
+      setRecipes([]);
+    }
+  };
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -44,50 +88,13 @@ const ProfilePage = (props) => {
       ).data;
       setUser({ ...additionData, ...userData });
     };
-    const fetchRecipes = async () => {
-      let recipesData;
-      try {
-        recipesData = (
-          await axios.get("НУЖЕН АДЕКВАТНЫЙ ЗАПРОС", {
-            headers: { Authorization: `Bearer ${token}` },
-          })
-        ).data;
-        //setRecipes(recipesData); ////////////////////////////////////////// ТУТ НАДО ИЗМЕНИТЬ ДАННЫЕ На ПРИХОДЯЩие
-        setRecipes([
-          {
-            id: 1,
-            name: "Яйца Бенедикт",
-            k: 123,
-            b: 321,
-            j: 142,
-            u: 23,
-          },
-          {
-            id: 2,
-            name: "Картошка по-деревенски в духовке",
-            k: 123,
-            b: 321,
-            j: 142,
-            u: 23,
-          },
-          {
-            id: 3,
-            name: "Сырники",
-            k: 123,
-            b: 321,
-            j: 142,
-            u: 23,
-          },
-        ]);
-      } catch (ex) {
-        const { response } = ex;
-        console.log(response);
-        setRecipes([]);
-      }
-    };
+
     fetchUser();
-    fetchRecipes();
   }, []);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, [startDate, endDate]);
 
   if (user === undefined) {
     return (
@@ -113,6 +120,25 @@ const ProfilePage = (props) => {
           <Content>
             <div className={styles.profile__form}>
               <ProfileForm user={user} />
+            </div>
+            <div className={styles.profile__date}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Начало"
+                  value={startDate}
+                  onChange={(newValue) =>
+                    setStartDate(newValue.hour(0).minute(0).second(0))
+                  }
+                />
+                <DatePicker
+                  sx={{ marginLeft: 2 }}
+                  label="Конец"
+                  value={endDate}
+                  onChange={(newValue) =>
+                    setEndDate(newValue.hour(23).minute(59).second(59))
+                  }
+                />
+              </LocalizationProvider>
             </div>
             <div className={styles.profile__table}>
               <ProfileTable rows={recipes} />
